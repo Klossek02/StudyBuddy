4ef87336bdc70e32681f7e157729c675a4c8edad
Aleksandra Kłos <67869082+Klossek02@users.noreply.github.com>
2024-05-07 12:04:45 +0200

    for tokenization purposes

---
+++ Models/AuthResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace StudyBuddy.Models
+{
+    public class AuthResponse
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime ExpiresIn { get; set; }
+    }
+}
