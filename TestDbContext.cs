a2342e9a3a0512fdd2fe465343c2324e995a7a90
Aleksandra Kłos <67869082+Klossek02@users.noreply.github.com>
2024-05-08 19:02:00 +0200

    Add files via upload

---
+++ TestDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudyBuddy;
+using StudyBuddy.Models;
+
+public class TestDbContext : IDisposable
+{
+    public ApplicationDbContext Context { get; private set; }
+
+    public TestDbContext()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "InMemoryDbForTesting")
+            .Options;
+
+        Context = new ApplicationDbContext(options);
+        Context.Database.EnsureCreated();
+        InitializeEntities();
+    }
+
+    private void InitializeEntities()
+    {
+        // Optionally add test data here, such as:
+        Context.Students.Add(new Student { FirstName = "Test", LastName = "User", Email = "test@example.com" });
+        Context.SaveChanges();
+    }
+
+    public void Dispose()
+    {
+        Context.Database.EnsureDeleted();
+        Context.Dispose();
+    }
+}
